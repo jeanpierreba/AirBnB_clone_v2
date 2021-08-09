b7b6f43d2917888ff57fb7d174a860a06ebaf7dd
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-09 15:02:15 -0500

    :heavy_plus_sign: Add advances of taks 6 with lib sqlalchemy

--- models/state.py
+++ models/state.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    # relationship between <State> and <City>
+    cities = relationship(
+        "City", backref="state", cascade="all, delete-orphan")
+
     name = ""
+
+    @property
+    def cities(self):
+        return self.__cities
