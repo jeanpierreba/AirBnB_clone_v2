bc8d67b4e91f88f135c8f1b59033280d2cd08a7a
jeanpierreba <2713@holbertonschool.com>
2021-08-09 11:07:52 -0500

    Updated all method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,11 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls:
+            return {key: obj for (key, obj) in self.__objects.items()
+                    if isinstance(obj, cls)}
         return FileStorage.__objects
 
     def new(self, obj):
@@ -36,23 +39,23 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
-	def delete(self, obj=None):
-		""" Deletes an object """
-		if obj:
-			obj_key = "{}.{}".format(type(obj).__name__, obj.id)
-			if (obj_key, obj) in self.__object.items():
-				self.__objects.pop(obj_key, None)
-		self.save()
+    def delete(self, obj=None):
+        """ Deletes an object """
+        if obj:
+            obj_key = "{}.{}".format(type(obj).__name__, obj.id)
+            if (obj_key, obj) in self.__object.items():
+                self.__objects.pop(obj_key, None)
+        self.save()
