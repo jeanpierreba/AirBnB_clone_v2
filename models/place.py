8e631e3ab9d375d7202c036eb3a13520cb3c4b51
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-09 21:26:14 -0500

    Update class place with attributes of ORM

--- models/place.py
+++ models/place.py
@@ -1,10 +1,31 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy.sql.expression import false, null
+from sqlalchemy.sql.sqltypes import Float
+from models.base_model import Base, BaseModel
+from sqlalchemy.orm import relation, relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Float
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
+    __tablename__ = 'places'
+
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=False)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
+
+    # relation = relationship(
+    #     "City", backref="state", cascade="all, delete-orphan")
+
     city_id = ""
     user_id = ""
     name = ""
--- models/user.py
+++ models/user.py
@@ -8,6 +8,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
