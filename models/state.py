9de5810e35c97ddbca248add6f13361bc2475024
jeanpierreba <2713@holbertonschool.com>
2021-08-10 11:09:08 -0500

    Added relationships

--- models/state.py
+++ models/state.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import Base, BaseModel
-from sqlalchemy import Column, Integer, String, ForeignKey
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
+from os import getenv
+import models
 
 
 class State(BaseModel, Base):
@@ -11,11 +13,18 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     # relationship between <State> and <City>
-    cities = relationship(
-        "City", backref="state", cascade="all, delete-orphan")
 
-    name = ""
-
-    @property
-    def cities(self):
-        return self.__cities
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """ returns the list of City instances with state_id
+            equals to the current State.id """
+            all_cities = models.storage.all(City)
+            state_city = []
+            for city_instance in all_cities.values():
+                    if city_instance.state_id == self.id:
+                        state_city.append(city_instance)
+            return state_city
