c51f65b665644891e64247cfba589a0e9b689800
Sergioarg <sergio_ramos53@yahoo.com>
2021-08-09 20:50:56 -0500

    :sparkles: Add of ORM with lib sqlalchemy

--- models/user.py
+++ models/user.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import Base, BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+
     email = ''
     password = ''
     first_name = ''
