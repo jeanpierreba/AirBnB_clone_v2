d636a92ed89043765c1d862659657a25683de382
jeanpierreba <2713@holbertonschool.com>
2021-08-10 16:17:58 -0500

    Removed nullable

--- models/place.py
+++ models/place.py
@@ -3,26 +3,25 @@
 from os import getenv
 import models
 from models.review import Review
-from sqlalchemy.sql.sqltypes import Float
-from models.base_model import Base, BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 
 class Place(BaseModel, Base):
-    """ A place to stay """
+    """ This class is for place attributes """
     __tablename__ = 'places'
 
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
+    description = Column(String(1024))
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
+    latitude = Column(Float)
+    longitude = Column(Float)
 
     if getenv("HBNB_TYPE_STORAGE") == 'db':
         reviews = relationship('Review', backref='place',
