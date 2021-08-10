6b8dc53c8fd9c408b6e2ca07f31fd0db6227021d
jeanpierreba <2713@holbertonschool.com>
2021-08-10 16:05:51 -0500

    Added relationships between review and user and place

--- models/place.py
+++ models/place.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from sqlalchemy.sql.expression import false, null
+from os import getenv
+import models
+from models.review import Review
 from sqlalchemy.sql.sqltypes import Float
 from models.base_model import Base, BaseModel
-from sqlalchemy.orm import relation, relationship
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Float
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 
 class Place(BaseModel, Base):
@@ -21,3 +23,18 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
+
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        reviews = relationship('Review', backref='place',
+                               cascade='all, delete-orphan')
+    else:
+        @property
+        def reviews(self):
+            """ returns the list of Review instances
+            with place_id equals to the current Place.id """
+            all_reviews = models.storage.all(Review)
+            place_review = []
+            for review_instances in all_reviews.values():
+                if review_instances.place_id == self.id:
+                    place_review.append(review_instances)
+            return place_review
