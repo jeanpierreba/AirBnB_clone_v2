ca611a383941042dca6e6b75217804d03ba304e4
jeanpierreba <2713@holbertonschool.com>
2021-08-11 10:32:53 -0500

    Removed conditional to amenity relationship

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from os import getenv
 from models.place import place_amenity
 from sqlalchemy.orm import relationship
 from models.base_model import Base, BaseModel
@@ -11,7 +10,4 @@ class Amenity(BaseModel, Base):
     """ This class is for amenity attributes """
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        place_amenities = relationship('Place',
-                                       secondary='place_amenity',
-                                       back_populates='amenities')
+    place_amenities = relationship('Place', secondary=place_amenity)
