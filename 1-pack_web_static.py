09995fbd4726af2efe9bb8162f0142199709905b
jeanpierreba <2713@holbertonschool.com>
2021-08-26 10:29:54 -0500

    Fixed pep8 requeriments

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,11 +7,11 @@ from datetime import datetime
 
 
 def do_pack():
-    """ Function to generate the .tgz file """
-    local("mkdir -p version")
-    archive = local("tar -cvzf versions/web_static_{}.tgz web_static"
-                    .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
-                    capture=True)
-    if archive.faile:
+    """ generates a .tgz archive from the contents of the web_static """
+    local("mkdir -p versions")
+    result = local("tar -cvzf versions/web_static_{}.tgz web_static"
+                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                   capture=True)
+    if result.failed:
         return None
-    return archive
+    return result
