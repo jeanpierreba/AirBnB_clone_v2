466bee1300714cb856e1e74d7be10951c8284e4b
jeanpierreba <2713@holbertonschool.com>
2021-08-26 10:32:27 -0500

    FIXED type error

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,11 +7,11 @@ from datetime import datetime
 
 
 def do_pack():
-    """ generates a .tgz archive from the contents of the web_static """
-    local("mkdir -p versions")
-    result = local("tar -cvzf versions/web_static_{}.tgz web_static"
-                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
-                   capture=True)
-    if result.failed:
+    """ Function to generate the .tgz file """
+    local("mkdir -p version")
+    archive = local("tar -cvzf versions/web_static_{}.tgz web_static"
+                    .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                    capture=True)
+    if archive.failed:
         return None
-    return result
+    return archive
