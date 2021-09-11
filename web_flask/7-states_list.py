bf333e9808a7451f1254d10812ad94f52441ab80
jeanpierreba <2713@holbertonschool.com>
2021-09-11 13:59:04 -0500

    Added list and cities files

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" starts a Flask web application """
+
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states():
+    """ Display an HTML page with states """
+    return render_template('7-states_list.html',
+                           states=storage.all('State').values())
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """ Removes SQLAlchemy session """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
